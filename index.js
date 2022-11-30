cacf47c81f17bff6aecb7f7dbefc26bebd2861d8
fjoralba29 <90421671+fjoralba29@users.noreply.github.com>
2022-11-30 13:56:07 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+import fsp from 'fs/promises';
+
+const content = await fsp.readFile('./data.csv', 'utf-8');
+
+async function fileDataType (content) {
+    const dataFile = content.split('\r\n');  
+    console.log(dataFile);
+    let sum = 0;
+    for (let data of dataFile) {
+        
+        let pattern = /[0-9]/g;
+        if (data.match(pattern)) {
+            data = data * 1; 
+            sum = sum + data; 
+            console.log(sum);  
+        }
+        else if (typeof data === 'string') {
+            const uniqueStrings = dataFile.filter((value, index, array) => array.indexOf(value) === index);
+            console.log(uniqueStrings);
+            break;
+        }
+        
+    }
+}
+
+await fileDataType(content); 
